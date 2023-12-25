8023bc62cae11ee19736f637c4b399b8b2349e7f
Anton Vasilev <6095724+tonycode@users.noreply.github.com>
2023-12-25 14:16:52 +0200

    feat(comida): BottomNav - Make items clickable

--- demos/comida-app/src/main/kotlin/dev/tonycode/composed/comida/ui/components/BottomNav.kt
+++ demos/comida-app/src/main/kotlin/dev/tonycode/composed/comida/ui/components/BottomNav.kt
@@ -1,6 +1,8 @@
 package dev.tonycode.composed.comida.ui.components
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,13 +10,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.tonycode.composed.comida.R
-import dev.tonycode.composed.comida.ui.screenHorizontalPadding
 import dev.tonycode.composed.comida.ui.theme.ComidaAppTheme
 import dev.tonycode.composed.comida.ui.theme.ComidaPalette
 
@@ -24,53 +29,67 @@ fun BottomNav(
     modifier: Modifier = Modifier,
 ) {
 
+    var selectedRoute by remember { mutableStateOf(navItems.first().route) }
+
     Surface(
         shape = MaterialTheme.shapes.medium,
         color = MaterialTheme.colorScheme.surface,
         modifier = modifier,
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
-            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
         ) {
-            Image(
-                painterResource(R.drawable.nav_home_28),
-                contentDescription = "Home screen",
-                colorFilter = ColorFilter.tint(ComidaPalette.Primary),
-            )
-
-            Image(
-                painterResource(R.drawable.nav_cart_28),
-                contentDescription = "Basket screen",
-                colorFilter = ColorFilter.tint(ComidaPalette.ParisPaving),
-            )
-
-            Image(
-                painterResource(R.drawable.nav_profile_28),
-                contentDescription = "Profile screen",
-                colorFilter = ColorFilter.tint(ComidaPalette.ParisPaving),
-            )
-
-            Image(
-                painterResource(R.drawable.nav_notifications_28),
-                contentDescription = "Notifications screen",
-                colorFilter = ColorFilter.tint(ComidaPalette.ParisPaving),
-            )
+            navItems.forEach { screen ->
+                BottomNavItem(
+                    screen,
+                    isSelected = (selectedRoute == screen.route),
+                    onSelected = { selectedRoute = screen.route },
+                )
+            }
         }
     }
 
 }
 
+@Composable
+private fun BottomNavItem(
+    screen: Screen,
+    isSelected: Boolean,
+    onSelected: () -> Unit,
+) {
+    Image(
+        painterResource(id = screen.iconRes),
+        contentDescription = screen.iconDescription,
+        colorFilter = ColorFilter.tint(
+            if (isSelected) ComidaPalette.Primary else ComidaPalette.ParisPaving
+        ),
+        modifier = Modifier.clickable { onSelected.invoke() }
+    )
+}
+
+private sealed class Screen(val route: String, @DrawableRes val iconRes: Int, val iconDescription: String) {
+    object Home : Screen("home", R.drawable.nav_home_28, "Home screen")
+    object Cart : Screen("cart", R.drawable.nav_cart_28, "Basket screen")
+    object Profile : Screen("profile", R.drawable.nav_profile_28, "Profile screen")
+    object Notifications : Screen("notifications", R.drawable.nav_notifications_28, "Notifications screen")
+}
+
+private val navItems = listOf(Screen.Home, Screen.Cart, Screen.Profile, Screen.Notifications)
+
+
 @Preview
 @Composable
-fun PreviewBottomNav() {
+private fun PreviewBottomNav() {
     ComidaAppTheme {
         Surface(
             color = MaterialTheme.colorScheme.background,
             modifier = Modifier.fillMaxWidth(),
         ) {
             BottomNav(
-                modifier = Modifier.padding(horizontal = screenHorizontalPadding)
+                modifier = Modifier.padding(24.dp)
             )
         }
     }
