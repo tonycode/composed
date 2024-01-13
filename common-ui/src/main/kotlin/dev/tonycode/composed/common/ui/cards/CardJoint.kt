c0b38f60f45bd57ca29fabdcdaa90aaf253f2767
Anton Vasilev <6095724+tonycode@users.noreply.github.com>
2024-01-13 09:12:48 +0200

    feat(common-ui): Add CardJoint enum with creation helper method

---
+++ common-ui/src/main/kotlin/dev/tonycode/composed/common/ui/cards/CardJoint.kt
@@ -0,0 +1,19 @@
+package dev.tonycode.composed.common.ui.cards
+
+
+enum class CardJoint {
+
+    /** A single, separate card */
+    Single,
+
+
+    /** Top card in a joined list */
+    Top,
+
+    /** Any intermediate card in a joined list */
+    Middle,
+
+    /** Bottom card in a joined list */
+    Bottom
+
+}
