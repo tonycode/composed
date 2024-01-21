731bc65e48e5d4489d43b009262420a56e3069a4
Anton Vasilev <6095724+tonycode@users.noreply.github.com>
2024-01-21 23:44:06 +0200

    build: Use Java 1.8 instead of 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,13 +48,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
 
         isCoreLibraryDesugaringEnabled = true  // https://developer.android.com/studio/write/java8-support#library-desugaring
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
 
     buildFeatures {
